b79a7e6ece9e8dec46d179d0f0c6a2b979f2f95f
bergabman <bergabman1@gmail.com>
2025-10-02 18:21:56 +0200

    update

--- benches/dashbench.rs
+++ benches/dashbench.rs
@@ -1,10 +1,10 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use dashbench::{
     dashmap_prealloc, dashmap_simple, dashmap_threaded, hashmap_prealloc, hashmap_simple,
-    hashmap_threaded, hashmap_threaded_rwlock,
+    hashmap_threaded, hashmap_threaded_rwlock, hashmap_range_query_simple
 };
 
-const ITER: usize = 1000;
+const ITER: usize = 9000;
 
 fn hashmap_bench(c: &mut Criterion) {
     let mut group = c.benchmark_group("HashMap");
@@ -34,7 +34,15 @@ fn rayon_bench(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(hashmap, hashmap_bench);
+fn hashmap_bench_range(c: &mut Criterion) {
+    let mut group = c.benchmark_group("HashMapRangeQuery");
+    group.bench_function("hashmap_range_query_simple", |b| b.iter(|| hashmap_simple(ITER)));
+
+    // group.bench_function("hashmap_prealloc", |b| b.iter(|| hashmap_prealloc(ITER)));
+    group.finish();
+}
+
+criterion_group!(hashmap, hashmap_bench, hashmap_bench_range);
 criterion_group!(dashmap, dashmap_bench);
 criterion_group!(rayon, rayon_bench);
 criterion_main!(hashmap, dashmap, rayon);
--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@ use std::{
 use dashmap::DashMap;
 use rayon::prelude::*;
 
+
+// item insert tests
 pub fn hashmap_simple(r_max: usize) {
     let mut benchmap = HashMap::new();
     let _ = (0..r_max).map(|num| benchmap.insert(num, num));
@@ -29,7 +31,7 @@ pub fn dashmap_prealloc(r_max: usize) {
 pub fn hashmap_threaded(r_max: usize) {
     let benchmap = Arc::new(Mutex::new(HashMap::new()));
 
-    let _ = (0..r_max).into_par_iter().for_each(|i| {
+    (0..r_max).into_par_iter().for_each(|i| {
         benchmap.lock().unwrap().insert(i, i);
     });
 }
@@ -37,7 +39,7 @@ pub fn hashmap_threaded(r_max: usize) {
 pub fn hashmap_threaded_rwlock(r_max: usize) {
     let benchmap = RwLock::new(HashMap::new());
 
-    let _ = (0..r_max).into_par_iter().for_each(|i| {
+    (0..r_max).into_par_iter().for_each(|i| {
         benchmap.write().unwrap().insert(i, i);
     });
 }
@@ -45,7 +47,13 @@ pub fn hashmap_threaded_rwlock(r_max: usize) {
 pub fn dashmap_threaded(r_max: usize) {
     let benchmap = DashMap::new();
 
-    let _ = (0..r_max).into_par_iter().for_each(|i| {
+    (0..r_max).into_par_iter().for_each(|i| {
         benchmap.insert(i, i);
     });
+}
+
+pub fn hashmap_range_query_simple(r_max: usize) {
+    let mut benchmap = HashMap::new();
+    let _ = (0..r_max).map(|num| benchmap.insert(num, num));
+    let _ = benchmap.iter().filter(|(k, _)| *k > &(r_max/2)).map(|(_, v)| *v).sum::<usize>();
 }
